145d17fc1b4ed7184fffae34d952634fc8edbca3
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2025-04-24 02:08:32 +0200

    g

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'product', component: ProductComponent },
+    { path: 'contact', component: ContactComponent},
+    { path: 'about', component: AboutComponent }
 
 ];
