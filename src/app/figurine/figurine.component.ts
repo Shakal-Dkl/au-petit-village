ba92889ce229c61f436895fb23f15463df00dac5
Shakal-Dkl <160112562+Shakal-Dkl@users.noreply.github.com>
2025-04-26 01:17:23 +0200

    petit avancé

---
+++ src/app/figurine/figurine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-figurine',
+  imports: [],
+  templateUrl: './figurine.component.html',
+  styleUrl: './figurine.component.css'
+})
+export class FigurineComponent {
+
+}
